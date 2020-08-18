59421c047f2cd139fb828d05c8b84e59589aecdb
codershona <falgunimst95@gmail.com>
2020-08-18 13:17:43 +0200

    just fcc api is working

--- src/App.js
+++ src/App.js
@@ -1,13 +1,65 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 // import './App.css';
 
-function App() {
+import axios from 'axios';
+
+
+
+class App extends Component {
+
+  state = {
+
+    top100Days: [],
+
+    top100AllTime: []
+
+
+  }
+
+ 
+ getFCCData(url, stateName) {
+
+  axios.get(url)
+   .then(({ data }) => {
+
+    this.setState({ [stateName]: data });
+    console.log(this.state.top100Days);
+
+    console.log(1);
+
+
+
+   })
+ }
+
+
+ componentDidMount() {
+
+  this.getFCCData('https://buttercup-island.glitch.me/latest', "top100Days");
+
+  this.getFCCData('https://buttercup-island.glitch.me/latest', "top100AllTime");
+
+
+ }
+
+
+
+
+render() {
+
   return (
+
     <div className="App">
-      <h1> the react</h1>
+
+
     </div>
-  );
+
+    );
+}
+
+
+
 }
 
 export default App;
