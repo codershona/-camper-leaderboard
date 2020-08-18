e11341a81e30790f0e42d6db86317287bed8524d
codershona <falgunimst95@gmail.com>
2020-08-18 15:14:36 +0200

    logic is not working

--- src/App.js
+++ src/App.js
@@ -4,8 +4,10 @@ import React, { Component } from 'react';
 
 import axios from 'axios';
 
-import Table from 'react-bootstrap/lib/Table';
+// import Table from 'react-bootstrap/lib/Table';
+import { Table } from 'react-bootstrap';
 
+import { Image } from 'react-bootstrap';
 
 
 class App extends Component {
@@ -14,7 +16,8 @@ class App extends Component {
 
     top100Days: [],
 
-    top100AllTime: []
+    top100AllTime: [],
+    current: true
 
 
   }
@@ -37,12 +40,21 @@ class App extends Component {
    })
  }
 
+ pointChange(value) {
+
+  if(this.state.current !== value) {
+    this.setState({current: value});
+
+
+  }
+ }
+
 
  componentDidMount() {
 
-  this.getFCCData('https://buttercup-island.glitch.me/latest', "top100Days");
+  this.getFCCData('https://fcctop100.herokuapp.com/api/fccusers/top/recent', "top100Days");
 
-  this.getFCCData('https://buttercup-island.glitch.me/latest', "top100AllTime");
+  this.getFCCData('https://fcctop100.herokuapp.com/api/fccusers/top/alltime', "top100AllTime");
 
 
  }
@@ -52,11 +64,79 @@ class App extends Component {
 
 render() {
 
+  const {top100Days, top100AllTime, current} = this.state;
+
   return (
 
-    <div className="App">
+    <div className="App container">
 
-    <Table> Test </Table>
+    <Table striped bordered condensed hover className="colorBlack"> 
+
+    <thread>
+
+    <tr>
+
+    <th>#</th>
+
+    <th>Camper Name</th>
+
+    <th onClick={(event) => this.pointChange(true)}>Points in 30 Days</th>
+
+    <th onClick={(event) => this.pointChange(false)}>All Time Points</th>
+
+    </tr>
+
+    </thread>
+
+    <tbody>
+
+     {current && (top100Days.maps((row, index) => (
+
+      <tr key={row.username}>
+
+      <td>{index + 1}</td>
+
+      <td>{`https://www.freecodecamp.org/${row.username}`}</td>
+
+      <Image src = {row.img} className="imgHeight" circle/> {row.username}
+
+      <td>{row.recent}</td>
+
+      <td>{row.alltime}</td>
+
+
+
+      </tr>
+
+     )
+     ))}
+
+
+     {current=== false && (top100AllTime.maps((row, index) => (
+
+      <tr key={row.username}>
+
+      <td>{index + 1}</td>
+
+      <td>{`https://www.freecodecamp.org/${row.username}`}</td>
+
+      <Image src = {row.img} className="imgHeight" circle/> {row.username}
+
+      <td>{row.recent}</td>
+
+      <td>{row.alltime}</td>
+
+
+
+      </tr>
+
+     )
+     ))}
+
+
+    </tbody>
+
+    </Table>
 
 
     </div>
